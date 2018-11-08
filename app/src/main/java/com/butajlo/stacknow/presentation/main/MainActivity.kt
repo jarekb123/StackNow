6d9122534abc335c4b1cc49a6d00fe8dc1655905
j.butajlo <j.butajlo@samsung.com>
2018-11-08 18:07:37 +0100

    Add toolbar with SearchView to MainActivity

--- app/src/main/java/com/butajlo/stacknow/presentation/main/MainActivity.kt
+++ app/src/main/java/com/butajlo/stacknow/presentation/main/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.butajlo.stacknow.presentation.main
 
+import android.app.SearchManager
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.findNavController
 import com.butajlo.stacknow.R
 import com.butajlo.stacknow.presentation.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.inject
 
 class MainActivity : BaseActivity() {
 
     override val layoutRes = R.layout.activity_main
+
+    private val searchManager: SearchManager by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+        (menu.findItem(R.id.menu_search).actionView as SearchView).apply {
+            maxWidth = Int.MAX_VALUE
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            setIconifiedByDefault(true)
+        }
+        return true
+    }
 
     override fun onBackPressed() {
         if(!findNavController(R.id.nav_host_fragment).navigateUp()) {
---
+++ app/src/main/java/com/butajlo/stacknow/presentation/search/SearchFragment.kt
@@ -0,0 +1,10 @@
+package com.butajlo.stacknow.presentation.search
+
+import com.butajlo.stacknow.R
+import com.butajlo.stacknow.presentation.base.BaseFragment
+
+class SearchFragment : BaseFragment() {
+
+    override val layoutRes = R.layout.search_screen
+
+}
