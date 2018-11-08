61f15e01dd523e42038268efadc966b1356c86f5
j.butajlo <j.butajlo@samsung.com>
2018-11-08 18:51:52 +0100

    fix:previous commit hadnt got code, start impl: QuestionBinding, SearchResultsAdapter

--- app/src/main/java/com/butajlo/stacknow/presentation/search/results/QuestionBinding.kt
+++ app/src/main/java/com/butajlo/stacknow/presentation/search/results/QuestionBinding.kt
@@ -1,2 +1,8 @@
 package com.butajlo.stacknow.presentation.search.results
 
+data class QuestionBinding(
+    val title: String,
+    val username: String,
+    val date: String,
+    val tags: List<String>
+)
--- app/src/main/java/com/butajlo/stacknow/presentation/search/results/SearchResultsAdapter.kt
+++ app/src/main/java/com/butajlo/stacknow/presentation/search/results/SearchResultsAdapter.kt
@@ -1,2 +1,9 @@
 package com.butajlo.stacknow.presentation.search.results
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchResultsAdapter {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
