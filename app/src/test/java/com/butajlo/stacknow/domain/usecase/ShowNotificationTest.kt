8f7350866497fdb17361de6b27c37afb2ba62e34
j.butajlo <j.butajlo@samsung.com>
2018-11-15 15:07:36 +0100

    Update show notifications use case

--- app/src/test/java/com/butajlo/stacknow/domain/usecase/ShowNotificationTest.kt
+++ app/src/test/java/com/butajlo/stacknow/domain/usecase/ShowNotificationTest.kt
@@ -2,6 +2,7 @@ package com.butajlo.stacknow.domain.usecase
 
 import com.butajlo.stacknow.domain.entity.NotificationEntity
 import com.butajlo.stacknow.domain.services.NotificationService
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import org.junit.Test
@@ -9,19 +10,11 @@ import org.junit.Test
 class ShowNotificationTest {
 
     private val notificationService = mock<NotificationService>()
-    private val notification = NotificationEntity(
-        id = 1,
-        title = "title",
-        content = "content",
-        importance = NotificationEntity.Importance.DEFAULT,
-        channelName = "channel",
-        isClosable = false
-    )
-
+    private val notification = mock<NotificationEntity>()
 
     @Test
     fun showNotification_ShouldCallShowOnNotificationService() {
         showNotification(service = notificationService, notification = notification)
-        verify(notificationService).show(notification)
+        verify(notificationService).show(any())
     }
 }
